c059e130f9d8baa85863b396c9e1966faaaf33d2
zhangzhixiong <zhangzhixiong2863@163.com>
2019-09-23 22:31:13 +0800

    请求工具

--- api/1.js
+++ api/1.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
-
+const bodyParser = require('body-parser');
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/test', function (req, res) {
     res.send('123')
 })
 app.post('/test', function (req, res) {
-    res.send('test post 123')
+    res.json(req.body)
+})
+app.delete('/test', function (req, res) {
+    res.json(req.body)
+})
+app.put('/test', function (req, res) {
+    res.json(req.body)
 })
 
-app.listen(3000)
+app.listen(8000)
 
-console.log('sever run at http://localhost:3000')
+console.log('sever run at http://localhost:8000')
