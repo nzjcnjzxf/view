c65f72ffbecdd115f918d068cadb34863313dfc9
Darren Zhang <darren_zhang@wistronits.com>
2019-10-12 14:45:07 +0800

    pug的使用

---
+++ item/vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    div.box
+      ul.wrap
+        li.item(v-text="msg" v-if="index > 2" v-for="(item,index) in 5" :key="index")
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.box {
+  .wrap {
+    .item {
+      color: red;
+    }
+  }
+}
+</style>
