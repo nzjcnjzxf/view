3fb6295bb6e0febb6278d4a9d0fa8ae2bae8f277
Darren Zhang <darren_zhang@wistronits.com>
2019-10-15 15:29:45 +0800

    vw

---
+++ demo/vw/src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <div class="home">home</div>
+    <div class="home home-1">home</div>
+  </div>
+</template>
+
+<style>
+.box {
+  display: flex;
+}
+.home {
+  width: 375px;
+  height: 375px;
+  background: red;
+}
+.home-1 {
+  background: blue;
+}
+</style>
